e35740381f7ecdfcde03d6c7f26f8830537db2f6
Mateus Brito <mateusbritocontact@gmail.com>
2023-08-16 18:40:29 -0300

    att

---
+++ src/study/Aula15AnatomiaClasses.java
@@ -0,0 +1,18 @@
+package study;
+
+public class Aula15AnatomiaClasses {
+    public static void main(String[] args) {
+        String firstName = "Mateus";
+        String secondName = "Mariana";
+
+        String fullName = fullName(firstName, secondName);
+
+        System.out.println(fullName);
+    }
+
+    public static String fullName(String firstName, String secondName) {
+        return firstName + " " + secondName;
+
+    }
+}
+
