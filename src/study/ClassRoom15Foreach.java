574ff483605445f70b1947cea4f5f0120c18f47c
Mateus Brito <mateusbritocontact@gmail.com>
2023-08-16 18:25:07 -0300

    att

---
+++ src/study/ClassRoom15Foreach.java
@@ -0,0 +1,12 @@
+package study;
+
+public class ClassRoom15Foreach {
+    public static void main(String[] args) {
+
+        int num = 10;
+        int num2 = 20;
+
+        System.out.println(num);
+    }
+}
+
