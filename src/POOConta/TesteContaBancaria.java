ed126dd26ae49ba59d606a76b69c023508654c4a
Mateus Brito <135028665+MateusBrito1@users.noreply.github.com>
2024-02-07 12:37:29 -0300

    ss
    ss

---
+++ src/POOConta/TesteContaBancaria.java
@@ -0,0 +1,28 @@
+package POOConta;
+
+public class TesteContaBancaria {
+
+
+    public static void main(String[] args) {
+
+        ContaBancaria contaBancaria1 = new ContaBancaria();
+        //Settando, estou definindo o numero e o titular etc... (Set = definindo)
+        contaBancaria1.setNumero("36547895499");
+
+        contaBancaria1.setTitular("José");
+        //Setando o saldo
+        contaBancaria1.setSaldo(100);
+
+        //Depositando o saldo, ficará 100 reais.
+        contaBancaria1.depositar(50);
+        contaBancaria1.sacar(100);
+
+        //Aqui eu estou dando get no número (get = pegando)
+        System.out.println(" Olá, " + contaBancaria1.getTitular());
+        System.out.println(" O número da conta é : " + contaBancaria1.getNumero());
+
+        System.out.println("SEILAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+
+
+    }
+}
