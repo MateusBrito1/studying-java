422c284af4a8dc3b8f9a790497c70fb58ef6b78e
Mateus Brito <mateusbritocontact@gmail.com>
2025-01-29 16:51:30 -0300

    docs: Aulas array

--- src/ProgramacaoEstruturada/Aula09Arrays.java
+++ src/ProgramacaoEstruturada/Aula09Arrays.java
@@ -4,6 +4,7 @@ public class Aula09Arrays {
     public static void main(String[] args) {
         int[] contador = new int [3];
         contador[0] = 21;
-        System.out.println(contador[0]);
+        contador[1] = 33;
+        System.out.println(contador[1]);
     }
 }
